9bc84ff752a1a7de6fdd9cccafd22f9c9b09d67a
Sandeep kumar <S.kumar943251@gmail.com>
2024-07-01 19:25:53 +0530

    first commit

---
+++ force-app/main/default/lwc/tribure_component/tribure_component.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import APJAbdulKalam from '@salesforce/resourceUrl/APJAbdul_Kalam';
+
+export default class Tribure_component extends LightningElement {
+    imageUrl = APJAbdulKalam
+}
